a3a6bae3a2f115bf2aa068573f01a58192a06288
KLW <weistrofferkl@s.dcsdk12.org>
2013-01-24 08:42:15 -0700

    Separated sum into method.

--- arrays.java
+++ arrays.java
@@ -1,12 +1,18 @@
 public class arrays{
 	public static void main(String[] args) {
 		int[]nums = {3,6,4,3,2,1,5,6};
+		System.out.println(average(nums));
 	}
 	public static double average(int[] nums){
+		
+		return ((double)sum(nums))/nums.length;
+	}
+	public static int sum(int[] nums){
 		int sum = 0;
-		for(int i=0; i<nums.length; i++){
-			sum += nums[i];
-		}
-		return ((double)sum)/nums.length;
+		for(int i= 0; i<nums.length, i++){
+			sum +=nums[i];
+
+		}return sum;
+
 	}
 }
